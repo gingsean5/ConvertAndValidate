cd9adc33ee0784265ff2ec6ac5ebe3caa82c702a
FroYoMan <34078793+FroYoMan@users.noreply.github.com>
2021-02-17 08:23:06 -0700

    fix everything

---
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -0,0 +1,27 @@
+﻿Module ConvertAndValidate
+
+    Sub Main()
+
+
+    End Sub
+    Private Function ValidateAndConvert(convertThisString As String, ByRef toThisInteger As Integer) As String
+        Dim message As String
+
+        Try
+            toThisInteger = CInt(convertThisString)
+
+            message = ""
+        Catch ex As Exception
+            If convertThisString = "" Then
+                message = "is empty"
+            Else
+                message = "must contain a number"
+            End If
+
+        End Try
+
+        Return message$
+
+
+    End Function
+End Module
